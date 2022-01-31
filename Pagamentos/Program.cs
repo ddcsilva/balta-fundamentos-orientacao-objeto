1e25f4c4a5f6b9946a38a7cf4d3954ce9ab10b63
Danilo Silva <danilo.silva@msn.com>
2022-01-31 09:43:32 -0300

    Aula sobre Modificadores de Acesso

--- Pagamentos/Program.cs
+++ Pagamentos/Program.cs
@@ -6,54 +6,25 @@ namespace Pagamentos
     {
         static void Main(string[] args)
         {
-            var pagamentoBoleto = new PagamentoBoleto();
-            pagamentoBoleto.Pagar();
-            pagamentoBoleto.Vencimento = DateTime.Now;
-            pagamentoBoleto.NumeroBoleto = "123";
-
-            var pagamentoCartaoCredito = new PagamentoCartaoCredito();
-            pagamentoCartaoCredito.Pagar();
-            pagamentoCartaoCredito.Vencimento = DateTime.Now;
-            pagamentoCartaoCredito.NumeroCartao = "321";
+            // private, protected, internal e public
 
             var pagamento = new Pagamento();
-            pagamento.Vencimento = Convert.ToDateTime("2022-01-01");
-            Console.WriteLine(pagamento.ToString());
         }
 
-        class Pagamento
+        public class Pagamento
         {
             // Propriedades
-            public DateTime Vencimento;
+            protected DateTime Vencimento;
 
             // Métodos
-            public virtual void Pagar() { }
-
-            public override string ToString()
-            {
-                return Vencimento.ToString("dd/MM/yyyy");
-            }
+            protected void Pagar() { }
         }
 
-        class PagamentoBoleto : Pagamento
+        public class PagamentoBoleto : Pagamento
         {
-            public string NumeroBoleto;
-
-            public override void Pagar()
+            void Test()
             {
-                // Regra do Boleto
-                Console.WriteLine("Pagamento Realizado com Boleto!");
-            }
-        }
-
-        class PagamentoCartaoCredito : Pagamento
-        {
-            public string NumeroCartao;
-
-            public override void Pagar()
-            {
-                // Regra do Cartão de Crédito
-                Console.WriteLine("Pagamento Realizado com Cartão de Crédito!");
+                base.
             }
         }
     }
