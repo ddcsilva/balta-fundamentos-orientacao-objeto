0d21bb9879b712a6fc3c88e0062b244e24aaf2ed
Danilo Silva <danilo.silva@msn.com>
2022-01-30 23:50:34 -0300

    Aula sobre Encapsulamento

---
+++ Pagamentos/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pagamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        class Pagamento
+        {
+            DateTime Vencimento;
+
+            void Pagar() { }
+        }
+    }
+}
